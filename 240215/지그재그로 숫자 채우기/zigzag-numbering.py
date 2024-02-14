7c232c404e4c3e814e8595c1561d135977cd4d09
minyoung <122516219+337ccc@users.noreply.github.com>
2024-02-15 00:46:19 +0900

    Title: 지그재그로 숫자 채우기, Time: 158ms, Memory: 25MB, Status: Passed - CodeTree

--- 240215/지그재그로 숫자 채우기/zigzag-numbering.py
+++ 240215/지그재그로 숫자 채우기/zigzag-numbering.py
@@ -3,9 +3,9 @@ n, m = map(int, input().split())
 arr = [[0] * m for _ in range(n)]
 for i in range(n):
     for j in range(m):
-        num = 7 + 8 * (j // 2)
+        num = n * (j + 1) - 1
         if j % 2 == 0:
-            arr[i][j] = 4 * j + i
+            arr[i][j] = n * j + i
         else:
             arr[i][j] = num - i
 
