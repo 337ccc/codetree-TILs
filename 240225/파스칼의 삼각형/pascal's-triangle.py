1c3587d7320a2c4e3480fe09e84b9a9279fa248d
minyoung <122516219+337ccc@users.noreply.github.com>
2024-02-25 16:10:55 +0900

    Title: 파스칼의 삼각형, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240225/파스칼의 삼각형/pascal's-triangle.py
+++ 240225/파스칼의 삼각형/pascal's-triangle.py
@@ -3,7 +3,7 @@ n = int(input())
 arr = [[1], [1, 1]]
 
 if n >= 3:
-    n = n - 1
+    n = n - 1  # 첫 번째 줄이 결국 0번째 인덱스이기 때문에 1을 빼고 시작
     for i in range(1, n):
         line = [1]
         for j in range(1, i + 1):
@@ -11,6 +11,7 @@ if n >= 3:
             line.append(num)
         line.append(1)
         arr.append(line)
+    n = n + 1. # 1 뺐던 걸 다시 더해줌
 
-for m in range(n + 1):
+for m in range(n):
     print(*arr[m])
