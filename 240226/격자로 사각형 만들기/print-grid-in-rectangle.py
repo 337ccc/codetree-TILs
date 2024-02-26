ea16d3c775ce91ff788dcbd529ea3e1739afa343
minyoung <122516219+337ccc@users.noreply.github.com>
2024-02-26 23:55:18 +0900

    Title: 격자로 사각형 만들기, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

--- 240226/격자로 사각형 만들기/print-grid-in-rectangle.py
+++ 240226/격자로 사각형 만들기/print-grid-in-rectangle.py
@@ -1,5 +1,9 @@
 n = int(input())
+
+# arr = [[1] * n] + [[1]] * (n - 1)라고 작성하면 안 됨
+# deep copy가 아닌 메모리를 공유하는 shallow copy가 진행되기 때문
 arr = [[1] * n] + [[1] for _ in range(n - 1)]
+
 for i in range(1, n):
     for j in range(1, n):
         num = arr[i - 1][j - 1] + arr[i - 1][j] + arr[i][j - 1]
