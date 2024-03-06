ec9d43c71a156b39668b68757585e37317a8fd22
minyoung <122516219+337ccc@users.noreply.github.com>
2024-03-06 11:44:58 +0900

    Title: 문자에 따른 명령 2, Time: 116ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240306/문자에 따른 명령 2/text-based-commands2.py
@@ -0,0 +1,26 @@
+order = list(input())
+
+now_y = 0
+now_x = 0
+
+# 북, 서, 남, 동
+dir_y = [-1, 0, 1, 0]
+dir_x = [0, -1, 0, 1]
+
+dir_order = 0
+for i in order:
+    if i == 'L':
+        if dir_order == 3:
+            dir_order = 0
+        else:
+            dir_order += 1
+    elif i == 'R':
+        if dir_order == 0:
+            dir_order = 3
+        else:
+            dir_order -= 1
+    else:
+        now_y += dir_y[dir_order]
+        now_x += dir_x[dir_order]
+
+print(now_x, now_y)
