da57345db34725ec0bd02375b6a72adb722d573e
minyoung <122516219+337ccc@users.noreply.github.com>
2024-03-09 00:03:55 +0900

    Title: 약수의 개수가 3개인 수, Time: 202ms, Memory: 25MB, Status: Passed - CodeTree

--- 240309/약수의 개수가 3개인 수/square-of-prime-number.py
+++ 240309/약수의 개수가 3개인 수/square-of-prime-number.py
@@ -6,8 +6,8 @@ for i in range(start, end + 1):
     for j in range(1, i + 1):
         if i % j == 0:
             divisor_count += 1
-            if divisor_count > 3:
-                continue
+            # if divisor_count > 3:
+            #     continue
     
     if divisor_count == 3:
         count += 1
