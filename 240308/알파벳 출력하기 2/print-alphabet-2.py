80790570c349dac318405f54c4d25f676f63cdf1
minyoung <122516219+337ccc@users.noreply.github.com>
2024-03-08 23:48:19 +0900

    Title: 알파벳 출력하기 2, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

--- 240308/알파벳 출력하기 2/print-alphabet-2.py
+++ 240308/알파벳 출력하기 2/print-alphabet-2.py
@@ -6,7 +6,10 @@ for i in range(n):
         print(' ', end=' ')
     
     for j in range(n - i, 0, -1):
-        num += 1
+        if num == 90:
+            num = 65
+        else:
+            num += 1
         print(chr(num), end=' ')
     
     print()
