b8d29c46bf887c56ef8e69ad25531ee54a632db1
minyoung <122516219+337ccc@users.noreply.github.com>
2024-03-13 23:24:35 +0900

    Title: 점수대 파악하기, Time: 144ms, Memory: 24MB, Status: Passed - CodeTree

--- 240313/점수대 파악하기/find-out-the-score-range.py
+++ 240313/점수대 파악하기/find-out-the-score-range.py
@@ -2,8 +2,11 @@ score = list(map(int, input().split()))
 
 ten = [0] * 11
 for i in score:
-    i //= 10
-    ten[i] += 1
+    if i == 0:
+        break
+    else:
+        i //= 10
+        ten[i] += 1
 
 for j in range(10, 0, -1):
     print(f'{10 * j} - {ten[j]}')
