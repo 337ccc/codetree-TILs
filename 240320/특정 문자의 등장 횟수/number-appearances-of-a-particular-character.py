eab863cf4fd6abf5ee61257c52783e5c921de352
minyoung <122516219+337ccc@users.noreply.github.com>
2024-03-20 00:17:50 +0900

    Title: 특정 문자의 등장 횟수, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240320/특정 문자의 등장 횟수/number-appearances-of-a-particular-character.py
@@ -0,0 +1,11 @@
+word = input()
+
+if 'ee' in word:
+    print(word.count('ee'), end=' ')
+else:
+    print(0, end=' ')
+
+if 'eb' in word:
+    print(word.count('eb'), end=' ')
+else:
+    print(0, end=' ')
