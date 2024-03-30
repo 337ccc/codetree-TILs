d1e5f360f6ba076b0b515adf04103be3f6e25a8f
minyoung <122516219+337ccc@users.noreply.github.com>
2024-03-30 19:19:36 +0900

    Title: 피보나치 수 2, Time: 5096ms, Memory: 31MB, Status: Time Limit Exceed - CodeTree

--- 240330/피보나치 수 2/fibonacci-number-3.py
+++ 240330/피보나치 수 2/fibonacci-number-3.py
@@ -1,9 +1,12 @@
+def fibonacci(n):
+    if n <= 1:
+        return n
+
+    a, b = 0, 1
+    for _ in range(2, n + 1):
+        a, b = b, a + b
+
+    return b % 1000000007
+
 n = int(input())
-fibo_lst = [0, 1]
-
-if n != 1:
-    for i in range(2, n + 1):
-        answer = fibo_lst[i - 2] + fibo_lst[i - 1]
-        fibo_lst.append(answer)
-
-print(answer % 1000000007)
+print(fibonacci(n))
