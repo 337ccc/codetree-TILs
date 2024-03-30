604913ae9e4e7dfecd31e392521551e322194988
minyoung <122516219+337ccc@users.noreply.github.com>
2024-03-30 19:22:18 +0900

    Title: 피보나치 수 2, Time: 141ms, Memory: 25MB, Status: Passed - CodeTree

--- 240330/피보나치 수 2/fibonacci-number-3.py
+++ 240330/피보나치 수 2/fibonacci-number-3.py
@@ -4,9 +4,9 @@ def fibonacci(n):
 
     a, b = 0, 1
     for _ in range(2, n + 1):
-        a, b = b, a + b
+        a, b = b, (a + b) % 1000000007
 
-    return b % 1000000007
+    return b
 
 n = int(input())
 print(fibonacci(n))
