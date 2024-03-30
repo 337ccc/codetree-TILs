ef900df718f3d6551f1f7026259f86cc91d15aa0
minyoung <122516219+337ccc@users.noreply.github.com>
2024-03-30 18:39:18 +0900

    Title: 스승의 은혜, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240330/스승의 은혜/the-grace-form-teacher.py
+++ 240330/스승의 은혜/the-grace-form-teacher.py
@@ -1,28 +1,31 @@
 def max_students(N, B, students):
-    students.sort(key=lambda x: (x[0], x[1]))  # 선물의 가격을 기준으로 학생들을 정렬
+    # 선물의 가격을 기준으로 올림차순으로 학생들을 정렬
+    students.sort(key=lambda x: (x[0], x[1]))
 
     total_cost = 0
     count = 0
 
     for i in range(N):
+        # 선물 가격, 배송비
         price, shipping = students[i]
-        total_cost += (price + shipping)  # 현재 학생이 선택된 경우의 총 비용에 가격을 추가
+        total_cost += (price + shipping)
 
-        # 선생님이 쿠폰을 사용하는 경우
-        if i % 2 == 1:
-            total_cost -= price / 2  # 가장 저렴한 선물에 대한 반값 할인
-
-        # 총 비용이 예산을 초과하는 경우, 이전 학생들을 선택해야 함
+        # 예산보다 총비용이 더 많이 나왔다면
         if total_cost > B:
-            break
+            # 반값 할인 적용해보기
+            total_cost -= price / 2
+            # 반값 할인 적용했는데도 예산보다 높다면 그 학생부턴 선물 주지 못함
+            if total_cost > B:
+                break
+            else:
+                count += 1
+                break
 
         count += 1
 
     return count
 
-# 입력 받기
 N, B = map(int, input().split())
 students = [list(map(int, input().split())) for _ in range(N)]
 
-# 결과 출력
 print(max_students(N, B, students))
