4f3817f9dd1bfcd964aded61f97644f6f9c25c60
minyoung <122516219+337ccc@users.noreply.github.com>
2024-04-01 23:48:20 +0900

    Title: 알파벳과 숫자 출력 2, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240401/알파벳과 숫자 출력 2/alphabet-and-numeric-output-2.py
+++ 240401/알파벳과 숫자 출력 2/alphabet-and-numeric-output-2.py
@@ -2,13 +2,20 @@ n = int(input())
 
 alpha_count = 64
 num_count = -1
+print(chr(90))
 for i in range(n + 1):
     for j in range(n - i, 0, -1):
-        alpha_count += 1
+        if alpha_count == 90:
+            alpha_count = 65
+        else:
+            alpha_count += 1
         print(chr(alpha_count), end=' ')
     
     for j in range(0, i):
-        num_count += 1
+        if num_count == 9:
+            num_count = 0
+        else:
+            num_count += 1
         print(num_count, end=' ')
-        
+
     print()
