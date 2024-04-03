d9c3ff994a0dba5cf47e06cb558e0ea88f5f518f
minyoung <122516219+337ccc@users.noreply.github.com>
2024-04-03 23:02:34 +0900

    Title: 이용권 효율적으로 구매하기, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240403/이용권 효율적으로 구매하기/purchasing-tickets-efficiently.py
+++ 240403/이용권 효율적으로 구매하기/purchasing-tickets-efficiently.py
@@ -1,4 +1,4 @@
-def min_cost(possible_days):
+def a_min_cost(possible_days):
     cost = 0
     coupon = 0
     
@@ -35,6 +35,39 @@ def min_cost(possible_days):
     
     return cost
 
+def b_min_cost(possible_days):
+    cost = 0
+    change_coupon = 0
+    for i in range(1, 99):
+        if i in possible_days:
+            count = 0
+            for j in range(i, i + 5):
+                if j in possible_days:
+                    count += 1
+            if count >= 4:
+                cost += 37000
+                change_coupon += 2
+                for j in range(i, i + 5):
+                    if j in possible_days:
+                        possible_days.remove(j)
+
+            elif i in possible_days and i + 1 in possible_days and i + 2 in possible_days:
+                cost += 25000
+                change_coupon += 1
+                for j in range(i, i + 3):
+                    possible_days.remove(j)
+
+            else:
+                if change_coupon != 3:
+                    cost += 10000
+                possible_days.remove(i)
+
+        else:
+            if len(possible_days) == 0:
+                return cost
+            else:
+                continue
+
 n, m = map(int, input().split())
 remove_days = list(map(int, input().split()))
 
@@ -42,5 +75,13 @@ possible_days = set(i for i in range(1, n + 1))
 for i in remove_days:
     possible_days.remove(i)
 
-answer = min_cost(possible_days)
-print(answer)
+a_possible_days = possible_days.copy()
+b_possible_days = possible_days.copy()
+
+answer_a = a_min_cost(a_possible_days)
+answer_b = b_min_cost(b_possible_days)
+
+if answer_a <= answer_b:
+    print(answer_a)
+else:
+    print(answer_b)
