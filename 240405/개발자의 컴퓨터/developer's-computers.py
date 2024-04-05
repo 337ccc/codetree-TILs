66e3f3f16c800aef400de638c7ce0911ce757605
minyoung <122516219+337ccc@users.noreply.github.com>
2024-04-05 16:06:43 +0900

    Title: 개발자의 컴퓨터, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240405/개발자의 컴퓨터/developer's-computers.py
@@ -0,0 +1,8 @@
+N = int(input())
+time_lst = list(0 for _ in range(1000))
+for _ in range(N):
+    s, t, b = map(int, input().split())
+    for i in range(s, t + 1):
+        time_lst[i] += b
+
+print(max(time_lst))
