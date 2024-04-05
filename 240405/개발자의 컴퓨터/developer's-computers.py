7772d8dce05f468739f6efbd157a18691b081f59
minyoung <122516219+337ccc@users.noreply.github.com>
2024-04-05 16:07:18 +0900

    Title: 개발자의 컴퓨터, Time: 118ms, Memory: 73MB, Status: Passed - CodeTree

--- 240405/개발자의 컴퓨터/developer's-computers.py
+++ 240405/개발자의 컴퓨터/developer's-computers.py
@@ -1,5 +1,5 @@
 N = int(input())
-time_lst = list(0 for _ in range(1000))
+time_lst = list(0 for _ in range(1001))
 for _ in range(N):
     s, t, b = map(int, input().split())
     for i in range(s, t + 1):
