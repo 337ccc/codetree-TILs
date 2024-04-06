5d3f623e0bfa1941f8d9aaf8a98e8b3765fe901a
minyoung <122516219+337ccc@users.noreply.github.com>
2024-04-06 23:58:32 +0900

    Title: 공약수, Time: 115ms, Memory: 25MB, Status: Passed - CodeTree

--- 240406/공약수/common-divisor.py
+++ 240406/공약수/common-divisor.py
@@ -1,7 +1,13 @@
 n = int(input())
-element_lst = list(map(int, input().split()))
-
-num = min(element_lst)
-for i in range(1, num + 1):
-    if num % i == 0:
-        print(i)
+if n == 2:
+    a, b = map(int, input().split())
+    num = min(a, b)
+    for i in range(1, num + 1):
+        if a % i == 0 and b % i == 0:
+            print(i)
+else:
+    a, b, c = map(int, input().split())
+    num = min(a, b, c)
+    for i in range(1, num + 1):
+        if a % i == 0 and b % i == 0 and c % i == 0:
+            print(i)
