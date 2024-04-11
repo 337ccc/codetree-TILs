e3ffc913756cfe9d623944bc050528a4a9ddc6e9
minyoung <122516219+337ccc@users.noreply.github.com>
2024-04-11 21:44:16 +0900

    Title: 지그재그로 이동하기, Time: 102ms, Memory: 24MB, Status: Passed - CodeTree

--- 240411/지그재그로 이동하기/moving-with-zigzag.py
+++ 240411/지그재그로 이동하기/moving-with-zigzag.py
@@ -1,10 +1,12 @@
 A, B = map(int, input().split())
 
+# A와 B의 거리
 gap = abs(A - B)
 now_dir = 'R'
 now = 1
 total = 1
 
+# A가 B를 향하는 방향
 if A - B < 0:
     dir = 'L'
 else:
@@ -12,7 +14,7 @@ else:
 
 if gap != 1:
     while now_dir == dir or gap > now:
-        if gap < now * 2:
+        if gap < now * 2 and dir == now_dir:
             total += (now + gap)
         else:
             total += (now + now * 2)
