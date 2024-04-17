50f7f01ab8d405513e6ea7c3003eca40c5713f8c
minyoung <122516219+337ccc@users.noreply.github.com>
2024-04-18 00:18:30 +0900

    Title: 순서대로 구구단 2, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240418/순서대로 구구단 2/multiplication-table-in-order-2.py
@@ -0,0 +1,19 @@
+a, b = map(int, input().split())
+
+if a <= b:
+    for i in range(a, b + 1):
+        num = 0
+        for _ in range(3):
+            for j in range(3):
+                num += 1
+                print(f'{i} * {num} = {i * num}', end='  ')
+        print()
+else:
+    for i in range(a, b - 1, -1):
+        num = 0
+        for _ in range(3):
+            for j in range(3):
+                num += 1
+                print(f'{i} * {num} = {i * num}', end='  ')
+            print()
+        print()
